ba363303df1577e4abc57e08c3c8afcea0064bdb
Syed Ifham <syedifham300@gmail.com>
2025-12-22 20:40:11 +0530

    path validation committed

--- organizer/cli/OrganizerCLI.java
+++ organizer/cli/OrganizerCLI.java
@@ -1,5 +1,55 @@
+//keep this file as short as possible
 package organizer.cli;
 
+import java.nio.file.*;
+import organizer.util.*;
+import organizer.core.*;
+
 public class OrganizerCLI {
-    
+
+  // stateless helper method
+  private static void printUsage() {
+    System.out.println("Use command : java OrganizerCLI <directory> --by <mode>");
+
+  }
+
+  public static void main(String[] args) {
+
+    // validating arguments length
+    if (args.length != 3) {
+      OrganizerCLI.printUsage();
+      return;
+    }
+
+    if (!args[1].equals("--by")) {
+      System.out.println("Input Error : Use --by");
+      OrganizerCLI.printUsage();
+      return;
+    }
+
+    // CLI inputs
+    String dir = args[0];
+    String mode = args[2];
+
+    // validating arguments mode
+    if (!mode.equals(OrganizerMode.extension) && !mode.equals("size")) {
+      System.out.println("Available Mode : extension or size");
+      printUsage();
+      return;
+    }
+
+    try {
+      // validates path
+      Path path = Paths.get(dir);
+      PathValidator.validate(path);
+
+    } catch (IllegalArgumentException exp) {
+      System.out.println("Arguments is/are wrong");
+    }
+
+    System.out.println("✔ Directory: " + dir);
+    System.out.println("✔ Mode: " + mode);
+
+  }
+
 }
---
+++ organizer/util/PathValidator.java
@@ -0,0 +1,12 @@
+package organizer.util;
+
+import java.nio.file.*;
+
+public class PathValidator {
+
+  // stateless utility 
+  public static void validate(Path path) {
+    
+  }
+
+}
